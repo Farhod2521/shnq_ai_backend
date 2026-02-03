6c66fb00a7108a0c3643800e885c15dc77b0b17b
Farhod2521 <abdikarimovfarhod86@gmail.com>
2026-02-03 16:35:44 +0500

    fixed

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [],
@@ -127,3 +128,4 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
+
